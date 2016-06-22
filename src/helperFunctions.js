5b280df0e6d12018a755c63eda320cbc881c21a8
Dan Kreiger <dan@dankreiger.com>
2016-06-22 19:44:53 +0200

    Update image urls

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -12,7 +12,7 @@ function helperFunctions() {
   this.evalIcon = function(iconText) {
     var icon = "wi ", backgroundPhoto = "";
     if(iconText == "01d"){icon += "wi-day-sunny";backgroundPhoto += "images/sunny.jpg";}
-    else if(iconText == "02d"){icon += "wi-day-cloudy";backgroundPhoto += "images/fewClouds.jpg";}
+    else if(iconText == "02d"){icon += "wi-day-cloudy";backgroundPhoto += "images/scatteredClouds.jpg";}
     else if(iconText == "03d"){icon += "wi-cloud";backgroundPhoto += "images/scatteredClouds.jpg";}
     else if(iconText == "04d"){icon += "wi-cloudy";backgroundPhoto += "images/brokenClouds.jpg";}
     else if(iconText == "09d"){icon += "wi-rain";backgroundPhoto += "images/lightRain.jpg"}
@@ -20,7 +20,7 @@ function helperFunctions() {
     else if(iconText == "11d"){icon += "wi-thunderstorm";backgroundPhoto += "images/thunderstorm.jpg"}
     else if(iconText == "13d"){icon += "wi-snow";backgroundPhoto += "images/snow.jpg"}
     else if(iconText == "50d"){icon += "wi-windy";backgroundPhoto += "images/windy.jpg"}
-    else if(iconText == "01n"){icon += "wi-night-clear";backgroundPhoto += "images/clearNight.jpg"}
+    else if(iconText == "01n"){icon += "wi-night-clear";backgroundPhoto += "images/clearNight.png"}
     else if(iconText == "02n"){icon += "wi-night-alt-cloudy";backgroundPhoto += "images/fewCloudsNight.jpg"}
     else if(iconText == "03n"){icon += "wi-cloud";backgroundPhoto += "images/scatteredCloudsNight.jpg"}
     else if(iconText == "04n"){icon += "wi-cloudy";backgroundPhoto += "images/brokenCloudsNight.jpg"}
